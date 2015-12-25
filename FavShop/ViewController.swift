6cb3905e8d0dcce025adb3577dbb3319d093b409
t-kitamura <kitamura.takuya@gmail.com>
2015-12-26 00:28:03 +0900

    プロジェクト作成

---
+++ FavShop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FavShop
+//
+//  Created by 北村拓也 on 2015/12/25.
+//  Copyright © 2015年 Chipstar Lignt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
